633b2c46f7413dd06c610e498ee0cd8af5c7e931
Oscar Flores <oscar.floresl@udem.edu>
2023-05-18 10:48:00 -0600

    nuevo dump

--- dump.sql
+++ dump.sql
@@ -27,6 +27,7 @@ CREATE TABLE `contrato_servicio` (
   `servicio_id` int(11) NOT NULL,
   `fecha_compra` date NOT NULL,
   `fecha_inicio` date NOT NULL,
+  `hora` int(11) NOT NULL,
   `nombre_titular` varchar(250) NOT NULL,
   `precio_compra` decimal(10,2) NOT NULL,
   PRIMARY KEY (`usuario_id`,`servicio_id`),
@@ -207,7 +208,7 @@ CREATE TABLE `reserva_habitacion` (
 
 LOCK TABLES `reserva_habitacion` WRITE;
 /*!40000 ALTER TABLE `reserva_habitacion` DISABLE KEYS */;
-INSERT INTO `reserva_habitacion` VALUES (1,101,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',3,8000.00),(1,103,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,104,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,105,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,201,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,202,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,203,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,204,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,205,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,301,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,302,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,303,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,304,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,305,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,401,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,402,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,403,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,404,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,405,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(2,102,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',3,8000.00);
+INSERT INTO `reserva_habitacion` VALUES (1,101,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',3,8000.00),(1,103,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,104,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,105,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,201,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,202,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,203,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,204,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,205,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,301,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,302,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,303,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,304,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,305,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,401,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,402,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,403,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,404,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(1,405,'2023-05-18','2023-06-10','2023-06-14','Oscar Flores',2,7000.00),(2,101,'2023-05-18','2023-07-10','2023-07-19','Oscar Flores',3,16000.00),(2,102,'2023-05-18','2023-06-10','2023-06-19','Oscar Flores',3,16000.00),(2,102,'2023-05-18','2023-07-10','2023-07-14','Oscar Flores',2,7000.00),(2,103,'2023-05-18','2023-07-10','2023-07-19','Oscar Flores',3,16000.00),(2,104,'2023-05-18','2023-07-10','2023-07-19','Oscar Flores',3,16000.00);
 /*!40000 ALTER TABLE `reserva_habitacion` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -286,7 +287,7 @@ CREATE TABLE `tipo_habitacion` (
 
 LOCK TABLES `tipo_habitacion` WRITE;
 /*!40000 ALTER TABLE `tipo_habitacion` DISABLE KEYS */;
-INSERT INTO `tipo_habitacion` VALUES (1,'Sencillo',1000.00),(2,'Doble',1500.00),(3,'Superior',2000.00);
+INSERT INTO `tipo_habitacion` VALUES (1,'Sencilla',1000.00),(2,'Doble',1500.00),(3,'Superior',2000.00);
 /*!40000 ALTER TABLE `tipo_habitacion` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -312,7 +313,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'oscarf17','Oscar Flores','pbkdf2:sha256:600000$nK0X0Jl8JBpM87mW$2e3df3700de4d9a8e8c782f7b7ab550654e90ef5cfa382564f933b03e3d442e9'),(2,'oscar','Oscar flores','pbkdf2:sha256:260000$DiwmrXCUWJuniOep$5779840f6fbde98794f1048dac903e5da42812ac7bf8741bb23a7531d520e259');
+INSERT INTO `usuario` VALUES (1,'oscarf17','Oscar Flores','pbkdf2:sha256:260000$Jv9FnSui5SnROakx$0b7306f9b77f9fdf1348059d9c623e739bd83e0d476e6d864602f6a619ce2798'),(2,'oscar','Oscar flores','pbkdf2:sha256:260000$DiwmrXCUWJuniOep$5779840f6fbde98794f1048dac903e5da42812ac7bf8741bb23a7531d520e259');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -325,4 +326,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-18  1:06:18
+-- Dump completed on 2023-05-18 10:47:41
